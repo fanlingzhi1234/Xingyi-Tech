511c061327599ceecd5d2d0e13557154cee96f8b
fanlingzhi1234 <cq.fanlingzhi@163.com>
2019-06-20 16:23:55 +0800

    daily update
    daily update

--- workplace/mybatisdemo/src/main/java/com/example/controller/UserController.java
+++ workplace/mybatisdemo/src/main/java/com/example/controller/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
